094c1607f6cb53b56acbf70d4e2786bdece82017
Anthony <92795688+lausan3@users.noreply.github.com>
2023-08-16 22:15:54 -0400

    Time: 152 ms (76.95%), Space: 98.6 MB (85.54%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,6 +2,68 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
+
+        // make a stack of pairs <temperature, day index>
+        stack<pair<int, int>> stk;
+
+        vector<int> answer(n, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int currTemp = temperatures[i];
+
+            // starting from the last item, check if the temperature at the top of the stack is 
+            // greater than the current temp, if so, subtract the indicies and put it in that day's slot in the 
+            // return vector, if not, pop the top of the stack, we won't need it anymore
+            while (!stk.empty() && stk.top().first <= currTemp) {
+                stk.pop();
+            }
+            
+            if (!stk.empty() && stk.top().first > currTemp) {
+                answer[i] = stk.top().second - i;
+            }
+
+            stk.push(pair<int, int>(currTemp, i));
+        }
+
+        return answer;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+        int n = temperatures.size();
         
         // pair: [index, temp]
         stack<pair<int, int>> stk;
@@ -26,5 +88,5 @@ public:
         }
         
         return result;
-    }
+    */
 };
