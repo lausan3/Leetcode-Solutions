7212ed4e5bcd1bd29d12712460b4645373064c59
Anthony <92795688+lausan3@users.noreply.github.com>
2024-02-20 13:55:17 -0500

    Time: 509 ms (91.80%), Space: 56.6 MB (35.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.scala
+++ 0001-two-sum/0001-two-sum.scala
@@ -3,17 +3,16 @@ import scala.collection.mutable.Map
 object Solution {
     def twoSum(nums: Array[Int], target: Int): Array[Int] = {
         val indexMap: Map[Int, Int] = Map()
-        val numsLength = nums.size
 
-        // Scala's for loop counters are inclusive??
-        for (x <- 0 to numsLength - 1) {
-            val num = nums(x)
-            val counterpart = target - num
+        def recurse(currIndex: Int): Array[Int] = {
+            if (currIndex >= nums.size) Array()
 
-            if (indexMap.contains(counterpart) == true) return Array(x, indexMap(counterpart))
-            else indexMap += (num -> x)
+            if (indexMap.contains(target - nums(currIndex)) == true) return Array(currIndex, indexMap(target - nums(currIndex)))
+            else indexMap += (nums(currIndex) -> currIndex)
+
+            recurse(currIndex + 1)
         }
-
-        Array()
+        
+        recurse(0)
     }
 }
