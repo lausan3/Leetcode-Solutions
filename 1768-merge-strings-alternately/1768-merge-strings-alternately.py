00d6846df9a28adf43e98ab6eecea6d9e34e62dc
Anthony <92795688+lausan3@users.noreply.github.com>
2024-05-29 22:40:59 -0400

    Time: 35 ms (56.60%), Space: 16.6 MB (50.17%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        # O(n) where n is the length of the smaller string
+        
+        res = []
+        oneLength, twoLength = len(word1), len(word2)
+
+        for i in range(max(oneLength, twoLength)):
+            if i < oneLength:
+                res.append(word1[i])
+            if i < twoLength:
+                res.append(word2[i])
+
+        return "".join(res)
