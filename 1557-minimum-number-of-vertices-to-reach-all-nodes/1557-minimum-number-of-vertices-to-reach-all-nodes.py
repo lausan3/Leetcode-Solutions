730bcee3b7bebf89200126abaaf069b0411de5e3
Anthony <92795688+lausan3@users.noreply.github.com>
2024-08-15 14:18:37 -0400

    Time: 923 ms (76.15%), Space: 54.9 MB (48.47%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -3,8 +3,11 @@ class Solution:
         # O(n)
         unreachableNodes = set([i for i in range(n)])
 
+        # O(e)
         for _, incomingNode in edges:
             if incomingNode in unreachableNodes:
                 unreachableNodes.remove(incomingNode)
+                
+        # time: O(n + e)
 
         return list(unreachableNodes)
