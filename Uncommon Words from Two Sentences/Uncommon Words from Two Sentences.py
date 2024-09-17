6e07d50f394620a393f9fa27f31a4fd3c34d6fb9
Anthony <92795688+lausan3@users.noreply.github.com>
2024-09-17 00:14:39 -0400

    COMMIT Uncommon Words from Two Sentences

--- Uncommon Words from Two Sentences/Uncommon Words from Two Sentences.py
+++ Uncommon Words from Two Sentences/Uncommon Words from Two Sentences.py
@@ -11,7 +11,7 @@ class Solution:
         # O(s2)
         for word in s2.split():
             s2_words[word] = s2_words.get(word, 0) + 1
-            
+
 
         for word, count in s1_words.items():
             if count == 1 and word not in result and word not in s2_words:
@@ -22,3 +22,17 @@ class Solution:
                 result.add(word)        
 
         return list(result)
+
+        # Cool solution I found on LeetCode:
+        # # Split the sentences into words
+        # words_s1 = s1.split()
+        # words_s2 = s2.split()
+        
+        # # Combine all words into one list
+        # all_words = words_s1 + words_s2
+        
+        # # Count the frequency of each word
+        # word_count = Counter(all_words)
+        
+        # # Find words that occur exactly once
+        # return [word for word in word_count if word_count[word] == 1]
