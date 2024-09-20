7517ccc6071bc9a58975576dfc45a837f733af7a
Anthony <92795688+lausan3@users.noreply.github.com>
2024-09-20 00:08:10 -0400

    COMMIT Dota2 Senate

--- Dota2 Senate/Dota2 Senate.py
+++ Dota2 Senate/Dota2 Senate.py
@@ -2,17 +2,24 @@ class Solution:
     def predictPartyVictory(self, senate: str) -> str:
         # Time: O(n). Space: O(n)
 
-        party_counts = Counter(senate)
-        i = 0
+        n = len(senate)
+        rad = deque()
+        dire = deque()
 
-        while party_counts["R"] > 0 and party_counts["D"] > 0:
-            if i >= len(senate):
-                i = 0
+        for i in range(len(senate)):
+            if senate[i] == "R":
+                rad.append(i)
+            else:
+                dire.append(i)
 
-            move = "D" if senate[i] == "R" else "R"
+        while rad and dire:
+            rad_senator = rad.popleft()
+            dire_senator = dire.popleft()
 
-            party_counts[move] -= 1
+            if rad_senator < dire_senator:
+                n += 1
+                rad.append(n)
+            else:
+                dire.append(n)
 
-            i += 1
-
-        return "Radiant" if party_counts["R"] > 0 else "Dire"
+        return "Radiant" if rad else "Dire"
