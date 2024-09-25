c4c9ee22d81b41fab62a35b51bf3228778cc7e23
Anthony <92795688+lausan3@users.noreply.github.com>
2024-09-25 13:39:21 -0400

    COMMIT Can Place Flowers

---
+++ Can Place Flowers/Can Place Flowers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowers_to_place = n
+
+        for i in range(len(flowerbed)):
+            if (flowerbed[i] == 0 and
+                (i - 1 < 0 or (i - 1 >= 0 and flowerbed[i-1] == 0)) and
+                (i + 1 >= len(flowerbed) or (i + 1 < len(flowerbed) and flowerbed[i+1] == 0))):
+                flowerbed[i] = 1
+                flowers_to_place -= 1
+
+            if flowers_to_place == 0:
+                return True
+
+        return False
