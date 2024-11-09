c69b8ef5f5e3d285fe91628fd44fe1066d71e8e2
Anthony <92795688+lausan3@users.noreply.github.com>
2024-11-09 15:35:14 -0500

    COMMIT Valid Palindrome

--- Valid Palindrome/Valid Palindrome.py
+++ Valid Palindrome/Valid Palindrome.py
@@ -2,15 +2,9 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         l, r = 0, len(s) - 1
 
-        sl = s.lower()
+        s = ''.join(filter(str.isalnum, s.lower()))
 
-        while l <= r:
-            while l < len(s) - 1 and not sl[l].isalnum():
-                l += 1
-
-            while r >= 0 and not sl[r].isalnum():
-                r -= 1
-
+        while l < r:
             if sl[l] != sl[r]:
                 return False
 
