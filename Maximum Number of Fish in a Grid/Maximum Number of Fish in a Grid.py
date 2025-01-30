c49f034cd62d64f7af6c580e18614c62757b0c3b
Anthony <92795688+lausan3@users.noreply.github.com>
2025-01-30 17:18:41 -0500

    COMMIT Maximum Number of Fish in a Grid

--- Maximum Number of Fish in a Grid/Maximum Number of Fish in a Grid.py
+++ Maximum Number of Fish in a Grid/Maximum Number of Fish in a Grid.py
@@ -3,23 +3,18 @@ class Solution:
         m, n = len(grid), len(grid[0])
         max_fish = 0
 
-        def dfs(r,c,curr):
-            if m < r < 0 or n < c < 0:
-                return curr
+        def dfs(r,c):
+            if (r >= m or r < 0 or c >= n or c < 0 or grid[r][c] <= 0):
+                return 0
+            
+            fish = grid[r][c]
+            grid[r][c] = -1
 
-            directions = [(1,0),(-1,0),(0,1),(0,-1)]
-
-            for ro, co in directions:
-                dr, dc = r + ro, c + co
-
-                if 0 <= dr < m and 0 <= dc < n and grid[r][c] > 0:
-                    return dfs(dr, dc, curr + grid[dr][dc])
-
-            return curr
+            return fish + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
 
         for r in range(m):
             for c in range(n):
                 if grid[r][c] > 0:
-                    max_fish = max(max_fish, dfs(r,c,grid[r][c]))
+                    max_fish = max(max_fish, dfs(r,c))
 
         return max_fish
