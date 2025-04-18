b91cf006e4bb24e024897f1273a3a60827424521
Anthony <92795688+lausan3@users.noreply.github.com>
2025-04-18 19:31:12 -0400

    COMMIT Average Selling Price

--- Average Selling Price/Average Selling Price.sql
+++ Average Selling Price/Average Selling Price.sql
@@ -1,10 +1,13 @@
 # Write your MySQL query statement below
 SELECT 
     p.product_id,
-    ROUND(SUM(p.price * us.units) / SUM(us.units), 2) AS average_price
-FROM UnitsSold us
-LEFT JOIN Prices p 
+    CASE WHEN COUNT(us.product_id = p.product_id)
+        THEN ROUND(SUM(p.price * us.units) / SUM(us.units), 2)
+        ELSE 0
+        END AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold us
     ON p.product_id = us.product_id
-WHERE us.purchase_date 
-    BETWEEN p.start_date AND p.end_date
+    AND us.purchase_date 
+        BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id;
