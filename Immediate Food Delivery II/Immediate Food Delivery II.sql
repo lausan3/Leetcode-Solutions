bb5fcdecb4c5aa5e03364965def35daff7f8d51f
Anthony <92795688+lausan3@users.noreply.github.com>
2025-04-21 00:01:05 -0400

    COMMIT Immediate Food Delivery II

--- Immediate Food Delivery II/Immediate Food Delivery II.sql
+++ Immediate Food Delivery II/Immediate Food Delivery II.sql
@@ -1,8 +1,17 @@
 # Write your MySQL query statement below
 SELECT
-    ROUND(AVG(IF(d.customer_pref_delivery_date = d.order_date, 100, 0)), 2) AS immediate_percentage
-FROM (
-    SELECT customer_id, min(order_date) AS order_date, customer_pref_delivery_date
-    FROM Delivery
-    GROUP BY customer_id
-) AS d;
+    ROUND(AVG(d.order_date = d.customer_pref_delivery_date) * 100, 2) AS immediate_percentage
+FROM
+    Delivery d
+INNER JOIN (
+    SELECT
+        customer_id,
+        MIN(order_date) AS first_order_date
+    FROM
+        Delivery
+    GROUP BY
+        customer_id
+) AS first_orders
+-- Join condition ensures we only consider the first order for each customer
+ON d.customer_id = first_orders.customer_id
+AND d.order_date = first_orders.first_order_date;
