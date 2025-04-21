8581e789d37758b7c2eb9d780ac02147929384bb
Anthony <92795688+lausan3@users.noreply.github.com>
2025-04-21 00:16:30 -0400

    COMMIT Game Play Analysis IV

---
+++ Game Play Analysis IV/Game Play Analysis IV.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+# From Editorial
+# Using CTEs (Temporary table created by WITH keyword), we can simplify the thought process
+#  and implemetation of this query.
+WITH first_logins AS (
+    SELECT 
+        a.player_id,
+        MIN(a.event_date) AS first_login
+    FROM Activity a
+    GROUP BY a.player_id
+), consec_logins AS (
+    SELECT
+        COUNT(a.player_id) AS num_logins
+    FROM
+        first_logins f
+        INNER JOIN Activity a ON f.player_id = a.player_id
+        AND f.first_login = DATE_SUB(a.event_date, INTERVAL 1 DAY)
+)
+SELECT
+    ROUND( (SELECT c.num_logins FROM consec_logins c) / (SELECT COUNT(f.player_id) FROM first_logins f), 2 ) AS fraction;
