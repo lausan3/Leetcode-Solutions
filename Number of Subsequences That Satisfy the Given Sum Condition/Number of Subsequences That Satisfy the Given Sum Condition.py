d955b0757ace7ae61e1433e5688752f200117ae5
Anthony <92795688+lausan3@users.noreply.github.com>
2025-06-29 18:45:53 -0400

    COMMIT Number of Subsequences That Satisfy the Given Sum Condition

---
+++ Number of Subsequences That Satisfy the Given Sum Condition/Number of Subsequences That Satisfy the Given Sum Condition.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        mod = 10 ** 9 + 7
+        n = len(nums)
+
+        # O(n log n)
+        nums.sort()
+
+        powers = [1] * n
+
+        for i in range(1, n):
+            powers[i] = powers[i - 1] * 2
+
+        l, r = 0, n - 1
+        count = 0
+
+        while l <= r:
+            if nums[l] + nums[r] <= target:
+                count += powers[r - l] % mod
+                l += 1
+            else:
+                r -= 1
+        
+        return count
