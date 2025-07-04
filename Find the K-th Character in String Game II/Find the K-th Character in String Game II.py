fd8bade02d1aa215679b65f97461dc10d9714dfd
Anthony <92795688+lausan3@users.noreply.github.com>
2025-07-04 16:29:30 -0400

    COMMIT Find the K-th Character in String Game II

--- Find the K-th Character in String Game II/Find the K-th Character in String Game II.py
+++ Find the K-th Character in String Game II/Find the K-th Character in String Game II.py
@@ -1,29 +1,41 @@
 class Solution:
     def kthCharacter(self, k: int, operations: List[int]) -> str:
-        word = "a"
+        # editorial solution:
+        ans = 0
+        while k != 1:
+            t = k.bit_length() - 1
+            if (1 << t) == k:
+                t -= 1
+            k -= 1 << t
+            if operations[t]:
+                ans += 1
+        return chr(ord("a") + (ans % 26))
 
-        def get_next_letter(letter: str) -> str:
-            a = ord('a')
-            next_letter = (ord(letter) +  1 - a) % 26
+        # old solution (not fast enough)
+        # word = "a"
 
-            return chr(next_letter + a)
+        # def get_next_letter(letter: str) -> str:
+        #     a = ord('a')
+        #     next_letter = (ord(letter) +  1 - a) % 26
 
-        for op in operations:
-            if len(word) > k:
-                break
+        #     return chr(next_letter + a)
 
-            if op == 0:
-                word += word
-            else:
-                shifted_word = ""
+        # for op in operations:
+        #     if len(word) > k:
+        #         break
 
-                for letter in word:
-                    next_letter = get_next_letter(letter)
-                    shifted_word += next_letter
+        #     if op == 0:
+        #         word += word
+        #     else:
+        #         shifted_word = ""
 
-                    if len(word) + len(shifted_word) > k:
-                        break
+        #         for letter in word:
+        #             next_letter = get_next_letter(letter)
+        #             shifted_word += next_letter
 
-                word += shifted_word
+        #             if len(word) + len(shifted_word) > k:
+        #                 break
 
-        return word[k - 1]
+        #         word += shifted_word
+
+        # return word[k - 1]
