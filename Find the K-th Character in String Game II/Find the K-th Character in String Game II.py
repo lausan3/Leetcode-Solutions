b27ab379ef7eae4368c2acf96439ba1f8325b44c
Anthony <92795688+lausan3@users.noreply.github.com>
2025-07-04 16:18:02 -0400

    COMMIT Find the K-th Character in String Game II

---
+++ Find the K-th Character in String Game II/Find the K-th Character in String Game II.py
@@ -0,0 +1,27 @@
+class Solution:
+    def kthCharacter(self, k: int, operations: List[int]) -> str:
+        word = "a"
+
+        def get_next_letter(letter: str) -> str:
+            a = ord('a')
+
+            return chr(a - ord(letter) + 1 % 26 + a)
+
+        for op in operations:
+            if len(word) > k:
+                break
+
+            if op == 0:
+                word += word
+            else:
+                shifted_word = ""
+
+                for letter in word:
+                    next_letter = get_next_letter(letter)
+                    shifted_word += next_letter
+
+                    print(f"{letter=}, {next_letter=}, {shifted_word=}")
+
+                word += shifted_word
+
+        return word[k - 1]
