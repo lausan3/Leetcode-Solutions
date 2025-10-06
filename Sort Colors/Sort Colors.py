ca9cf41d5a371d16497db2c58ca49a26f6feda73
Anthony <92795688+lausan3@users.noreply.github.com>
2025-10-06 16:49:23 -0400

    ADD Sort Colors

--- Sort Colors/Sort Colors.py
+++ Sort Colors/Sort Colors.py
@@ -3,18 +3,35 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Simple frequency array O(2n) time O(1) space solution
-        freq = [0] * 3
+        # 1. Simple frequency array O(2n) time O(1) space solution
+        # freq = [0] * 3
         
-        for color in nums:
-            freq[color] += 1
+        # for color in nums:
+        #     freq[color] += 1
 
-        # populate colors in place
-        # O(n) since freq's values add up to the amount of colors in nums
-        i = 0
-        for color, color_freq in enumerate(freq):
-            for _ in range(color_freq):
-                nums[i] = color
-                i += 1
+        # # populate colors in place
+        # # O(n) since freq's values add up to the amount of colors in nums
+        # i = 0
+        # for color, color_freq in enumerate(freq):
+        #     for _ in range(color_freq):
+        #         nums[i] = color
+        #         i += 1
 
-        # More complex three pointer O(n) time true O(1) space solution
+        # 2. More complex three pointer O(n) time true O(1) space solution
+        # pointers for where to swap numbers
+        r, w, b = 0, 0, len(nums) - 1
+
+        while w <= b:
+            if w < r and nums[w] == 0:
+                w += 1
+
+            curr = nums[w]
+
+            if curr == 0:
+                nums[r], nums[w] = nums[w], nums[r]
+                r += 1
+            elif curr == 1:
+                w += 1
+            else:
+                nums[b], nums[w] = nums[w], nums[b]
+                b -= 1
