97bc471fd0c6622d71517be122378e2b65fbeeff
Anthony <92795688+lausan3@users.noreply.github.com>
2025-10-10 15:10:18 -0400

    ADD Shortest Path in Binary Matrix

--- Shortest Path in Binary Matrix/Shortest Path in Binary Matrix.py
+++ Shortest Path in Binary Matrix/Shortest Path in Binary Matrix.py
@@ -16,11 +16,11 @@ class Solution:
         while q:
             curr_r, curr_c, path_length = q.popleft()
 
+            if curr_r == m - 1 and curr_c == n - 1:
+                return path_length
+
             for r, c in directions:
                 dr, dc = curr_r + r, curr_c + c
-
-                if dr == m - 1 and dc == n - 1:
-                    return path_length + 1
 
                 if (
                         0 <= dr < m and
