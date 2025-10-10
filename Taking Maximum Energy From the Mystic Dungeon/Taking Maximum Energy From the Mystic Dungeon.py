5f79b122de0615ef5dbb1e42257480ad74c647b5
Anthony <92795688+lausan3@users.noreply.github.com>
2025-10-10 14:38:00 -0400

    ADD Taking Maximum Energy From the Mystic Dungeon

--- Taking Maximum Energy From the Mystic Dungeon/Taking Maximum Energy From the Mystic Dungeon.py
+++ Taking Maximum Energy From the Mystic Dungeon/Taking Maximum Energy From the Mystic Dungeon.py
@@ -1,14 +1,14 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         # O(log n) binary search solution
-        n = len(heights)
+        n = len(arr)
         l, r = 0, n - 1
 
         while l < r:
             m = (l + r) // 2
 
             # peak is after middle - search right half
-            if heights[m] < heights[m + 1]:
+            if arr[m] < arr[m + 1]:
                 l = m + 1
             # peak is before middle - search left half
             else:
