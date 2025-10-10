b3e0476e12e29178a04f77d6b0bd439a17b6e1ac
Anthony <92795688+lausan3@users.noreply.github.com>
2025-10-10 15:18:57 -0400

    ADD Shortest Path in Binary Matrix

--- Shortest Path in Binary Matrix/Shortest Path in Binary Matrix.py
+++ Shortest Path in Binary Matrix/Shortest Path in Binary Matrix.py
@@ -10,8 +10,9 @@ class Solution:
         if grid[0][0] == 0:
             q.append((0, 0, 1))
 
-        # down, downright, right
-        directions = [(1, 0), (1, 1), (0, 1)]
+        # down, downleft, downright, right, upleft, upright
+        directions = [(1, 0), (1, -1), (1, 1), (0, 1), (-1, -1), (-1, 1)]
+        visited: set[tuple[int, int]] = set()
 
         while q:
             curr_r, curr_c, path_length = q.popleft()
@@ -19,12 +20,15 @@ class Solution:
             if curr_r == m - 1 and curr_c == n - 1:
                 return path_length
 
+            visited.add((curr_r, curr_c))
+
             for r, c in directions:
                 dr, dc = curr_r + r, curr_c + c
 
                 if (
                         0 <= dr < m and
                         0 <= dc < n and
+                        (dr, dc) not in visited and
                         grid[dr][dc] == 0
                 ):
                     q.append((dr, dc, path_length + 1))
