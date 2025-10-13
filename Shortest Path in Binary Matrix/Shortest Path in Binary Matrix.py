64daa8049ceaa7b7fa7d15e764c19f390aeb9ff8
Anthony <92795688+lausan3@users.noreply.github.com>
2025-10-13 13:15:25 -0400

    ADD Shortest Path in Binary Matrix

--- Shortest Path in Binary Matrix/Shortest Path in Binary Matrix.py
+++ Shortest Path in Binary Matrix/Shortest Path in Binary Matrix.py
@@ -5,40 +5,38 @@ class Solution:
         # BFS Solution
         m, n = len(grid), len(grid[0])
 
+        if grid[0][0] != 0 or grid[m-1][n-1] != 0:
+            return -1
+        if n == 1:
+            return 1
+
         q: deque[tuple[int, int, int]] = deque()
 
-        if grid[0][0] == 0:
-            q.append((0, 0, 1))
+        visited: set[tuple[int, int]] = set()
 
-            [
-                [0,1,1,0,1],
-                [0,1,0,1,0],
-                [0,1,0,1,0],
-                [1,0,1,1,0],
-                [1,1,1,1,0]
-            ]
+        q.append((0, 0, 1))
 
         # down, downleft, downright, right, upleft, upright, up, left
         directions = [(1, 0), (1, -1), (1, 1), (0, 1), (-1, -1), (-1, 1), (-1, 0), (0, -1)]
-        visited: set[tuple[int, int]] = set()
+        
 
         while q:
             curr_r, curr_c, path_length = q.popleft()
-
-            if curr_r == m - 1 and curr_c == n - 1:
-                return path_length
 
             visited.add((curr_r, curr_c))
 
             for r, c in directions:
                 dr, dc = curr_r + r, curr_c + c
 
+                if dr == m - 1 and dc == n - 1:
+                    return path_length + 1
+
                 if (
                         0 <= dr < m and
                         0 <= dc < n and
-                        (dr, dc) not in visited and
                         grid[dr][dc] == 0
                 ):
                     q.append((dr, dc, path_length + 1))
+                    grid[dr][dc] = -1
 
         return -1
