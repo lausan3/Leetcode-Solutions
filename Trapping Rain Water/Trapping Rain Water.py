9a60d730e43b96f09ead347bf74daa81bb394700
Anthony <92795688+lausan3@users.noreply.github.com>
2025-10-16 15:33:56 -0400

    ADD Trapping Rain Water

---
+++ Trapping Rain Water/Trapping Rain Water.py
@@ -0,0 +1,18 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # O(n) Time, O(1) Space
+        l, r = 0, len(height) - 1
+        max_l, max_r = height[l], height[r]
+        water_count = 0
+
+        while l < r:
+            if max_l < max_r:
+                l += 1
+                max_l = max(max_l, height[l])
+                water_count += max_l - height[l]
+            else:
+                r -= 1
+                max_r = max(max_r, height[r])
+                water_count += max_r - height[r]
+                
+        return water_count
