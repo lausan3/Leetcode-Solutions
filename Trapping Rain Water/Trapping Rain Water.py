b7ce574cbf927be8e972d67d7bd78bd41cbeb735
Anthony <92795688+lausan3@users.noreply.github.com>
2025-10-16 15:35:38 -0400

    ADD Trapping Rain Water

--- Trapping Rain Water/Trapping Rain Water.py
+++ Trapping Rain Water/Trapping Rain Water.py
@@ -10,6 +10,7 @@ class Solution:
                 l += 1
                 max_l = max(max_l, height[l])
                 water_count += max_l - height[l]
+                # because we did max then add to count, new maxes won't count toward water count
             else:
                 r -= 1
                 max_r = max(max_r, height[r])
