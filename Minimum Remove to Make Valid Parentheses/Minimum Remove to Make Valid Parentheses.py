d3774b3164b4f0dc4021221522440ad8b563d141
Anthony <92795688+lausan3@users.noreply.github.com>
2025-10-16 16:46:17 -0400

    ADD Minimum Remove to Make Valid Parentheses

--- Minimum Remove to Make Valid Parentheses/Minimum Remove to Make Valid Parentheses.py
+++ Minimum Remove to Make Valid Parentheses/Minimum Remove to Make Valid Parentheses.py
@@ -7,9 +7,10 @@ class Solution:
         parens = []
 
         for i, char in enumerate(chars):
-            if (char == "(" or 
-               (char == ")" and (not parens or (parens and parens[-1][1] != "(")))
-            ):
+            if char == "(":
+                parens.append((i, char))
+            # char is ) and not matched correctly
+            elif char == ")" and (not parens or (parens and parens[-1][1] != "(")):
                 parens.append((i, char))
             elif char == ")" and parens and parens[-1][1] == "(":
                 parens.pop()
