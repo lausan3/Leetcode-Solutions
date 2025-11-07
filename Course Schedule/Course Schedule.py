8cba2accd077d398343c63cb0908b9f57326e00e
Anthony <92795688+lausan3@users.noreply.github.com>
2025-11-06 22:30:26 -0500

    ADD Course Schedule

--- Course Schedule/Course Schedule.py
+++ Course Schedule/Course Schedule.py
@@ -22,34 +22,34 @@ class Solution:
         Time: O(V + E)
         Space: O(V)
         """
-        # adj_list = { i : set() for i in range(numCourses) }
-        # indegrees = [0] * numCourses
+        adj_list = { i : set() for i in range(numCourses) }
+        indegrees = [0] * numCourses
 
-        # # build adjacency list
-        # for course, prereq in prerequisites:
-        #     adj_list[prereq].add(course)
-        #     indegrees[course] += 1
+        # build adjacency list
+        for course, prereq in prerequisites:
+            adj_list[prereq].add(course)
+            indegrees[course] += 1
 
-        # visit_count = 0
-        # q = deque()
+        visit_count = 0
+        q = deque()
 
-        # # add initial 0 indegrees
-        # q.extend([i for i in range(numCourses) if indegrees[i] == 0])
+        # add initial 0 indegrees
+        q.extend([i for i in range(numCourses) if indegrees[i] == 0])
 
-        # # This is Kahn's algorithm
-        # while q:
-        #     prereq = q.popleft()
-        #     visit_count += 1
+        # This is Kahn's algorithm
+        while q:
+            prereq = q.popleft()
+            visit_count += 1
             
-        #     courses = adj_list[prereq]
+            courses = adj_list[prereq]
             
-        #     for course in courses:
-        #         indegrees[course] -= 1
+            for course in courses:
+                indegrees[course] -= 1
 
-        #         if indegrees[course] == 0:
-        #             q.append(course)
+                if indegrees[course] == 0:
+                    q.append(course)
 
-        # return visit_count == numCourses
+        return visit_count == numCourses
 
         """
         The DFS version of topological sort involves going through courses and their prereqs
@@ -77,31 +77,31 @@ class Solution:
             
             return true if we haven't returned false yet
         """
-        adj_list = { i : [] for i in range(numCourses)}  # course number : prerequesites
+        # adj_list = { i : [] for i in range(numCourses)}  # course number : prerequesites
 
-        for course, prereqs in prerequisites:
-            adj_list[course].append(prereqs)
+        # for course, prereqs in prerequisites:
+        #     adj_list[course].append(prereqs)
 
-        # visit set to detect cycles.
-        # if a prerequisite is in the set, then it's a loop.
-        visited = set()
+        # # visit set to detect cycles.
+        # # if a prerequisite is in the set, then it's a loop.
+        # visited = set()
 
-        def dfs(course):
-            if course in visited:
-                return False
-            if adj_list[course] == []:
-                return True
+        # def dfs(course):
+        #     if course in visited:
+        #         return False
+        #     if adj_list[course] == []:
+        #         return True
             
-            visited.add(course)
+        #     visited.add(course)
 
-            for pre in adj_list[course]:
-                if not dfs(pre): return False
+        #     for pre in adj_list[course]:
+        #         if not dfs(pre): return False
 
-            visited.remove(course)
-            adj_list[course] = []   # visited all course prereqs, so reset the prereqs list
-            return True
+        #     visited.remove(course)
+        #     adj_list[course] = []   # visited all course prereqs, so reset the prereqs list
+        #     return True
 
-        for course in range(numCourses):
-            if not dfs(course): return False
+        # for course in range(numCourses):
+        #     if not dfs(course): return False
 
-        return True
+        # return True
