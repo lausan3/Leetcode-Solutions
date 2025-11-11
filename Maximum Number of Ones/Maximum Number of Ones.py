90f4ce6695b6a5058157addc6ceeb9cd9e3fce65
Anthony <92795688+lausan3@users.noreply.github.com>
2025-11-11 17:35:28 -0500

    ADD Maximum Number of Ones

---
+++ Maximum Number of Ones/Maximum Number of Ones.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumNumberOfOnes(self, width: int, height: int, sideLength: int, maxOnes: int) -> int:
+        """
+        Greedy mathematical approach (from editorial):
+
+        Time: O(m * n
+        )
+        """
