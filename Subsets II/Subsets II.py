bc4a7f48ccf113b2c8bcac35ab207c4ffb7afc45
Anthony <92795688+lausan3@users.noreply.github.com>
2025-11-13 17:16:08 -0500

    ADD Subsets II

---
+++ Subsets II/Subsets II.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+
+        res = []
+        curr = []
+
+        def backtrack(i: int) -> None:
+            if i == n:
+                if curr not in res:
+                    res.append(curr.copy())
+                return
+
+            curr.append(nums[i])
+            backtrack(i + 1)
+
+            curr.pop()
+            backtrack(i + 1)
+
+        backtrack(0)
+
+        return res
