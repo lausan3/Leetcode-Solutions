62daf1aba5a0e27c22fa193f58ec3d58fc2f5b51
Anthony <92795688+lausan3@users.noreply.github.com>
2025-11-17 18:57:22 -0500

    ADD Maximal Square

--- Maximal Square/Maximal Square.py
+++ Maximal Square/Maximal Square.py
@@ -1,43 +1,36 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         """
-        Optimal bottom-up dynamic programming solution - Given by Cedric at GSWEP
+        Dynamic Programming Bottom-up solution:
 
-        The approach is to gradually build out a matrix containing the size of the largest
-         possible square side length created for each cell incrementally using dynamic
-         programming.
+        For each entry in the matrix, check if 1
+            if 1, check the top, left, and top left items in matrix are 1. If so, dp[r][c] = min(dp left, dp top, dp top left) + 1.
 
-        For each cell, if it's a cell we want to keep track of, check the top, left, and
-         top left cells for 1s. If we can construct this square, store the size of the
-         max square that can be created by adding the minimum of these three stored values
-         + 1 to include itself.
-
-        # Time: O(m * n)
-        # Space: O(m * n)
+        Time: O(m * n)
+        Space: O(m * n)
         """
-        m, n = len(matrix), len(matrix[0])
-        memo = [[0] * n for _ in range(m)]
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        dp = [[0] * n for _ in range(m)]
 
-        max_length = 0
+        max_sides = 0
 
-        # O(m * n) time
         for r in range(m):
             for c in range(n):
-                cell = matrix[r][c]
-
-                if cell != "1":
+                if matrix[r][c] == "0":
                     continue
 
-                if (
-                    r > 0 and c > 0 and
-                    matrix[r-1][c] == "1" and # top
-                    matrix[r][c-1] == "1" and # left
-                    matrix[r-1][c-1] == "1"   # top left
-                ):
-                    memo[r][c] = min(memo[r-1][c], memo[r][c-1], memo[r-1][c-1]) + 1
-                else:
-                    memo[r][c] = 1
+                if r <= 0 or c <= 0:
+                    dp[r][c] = 1
+                    max_sides = max(max_sides, 1)
 
-                max_length = max(max_length, memo[r][c])
-        
-        return max_length * max_length
+                left = int(matrix[r][c - 1])
+                top = int(matrix[r - 1][c])
+                top_left = int(matrix[r - 1][c - 1])
+                
+                dp[r][c] = min(dp[r][c - 1], dp[r - 1][c], dp[r - 1][c - 1]) + 1
+
+                max_sides = max(max_sides, dp[r][c])
+
+        return max_sides ** 2
