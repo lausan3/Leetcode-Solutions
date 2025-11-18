953d9804725795ffb69ab02e6d17766afa970424
Anthony <92795688+lausan3@users.noreply.github.com>
2025-11-17 21:10:23 -0500

    ADD 1-bit and 2-bit Characters

--- 1-bit and 2-bit Characters/1-bit and 2-bit Characters.py
+++ 1-bit and 2-bit Characters/1-bit and 2-bit Characters.py
@@ -1,8 +1,3 @@
 class Solution:
     def isOneBitCharacter(self, bits: List[int]) -> bool:
-        odd = len(bits) % 2 == 1
-
-        if odd:
-            return True
-        else:
-            return bits[-2] == 0
+        bits[-2] == 0
