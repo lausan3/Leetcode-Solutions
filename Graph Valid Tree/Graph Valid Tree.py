91c27eb51eb193eea7bc9606d46c735344f51382
Anthony <92795688+lausan3@users.noreply.github.com>
2025-11-17 22:36:49 -0500

    ADD Graph Valid Tree

---
+++ Graph Valid Tree/Graph Valid Tree.py
@@ -0,0 +1,8 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        indegree = [0] * n
+
+        for _, end in edges:
+            indegree[end] += 1
+
+        return sum(indegree) == n - 1
