e88a428b7b2b44cf775c4ef1462dddafa7538b20
Anthony <92795688+lausan3@users.noreply.github.com>
2025-11-17 23:25:22 -0500

    ADD Number of Connected Components in an Undirected Graph

--- Number of Connected Components in an Undirected Graph/Number of Connected Components in an Undirected Graph.py
+++ Number of Connected Components in an Undirected Graph/Number of Connected Components in an Undirected Graph.py
@@ -1,28 +1,44 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         """
-        My attempt at a primitive union find algorithm:
+        Simple DFS Solution:
+
+        init adj list
+
+        init visited set
+        init connected components counter
+
+        for each node
+            if node not visited
+                increment counter
+
+                add node to visited
+
+                visit each neighbor
+
+        Time: O(V + E)
+        Space: O(V + E]
         """
-        N_MAX = 2001
-        roots = [N_MAX for _ in range(n)]
-        in_adj = [set() for _ in range(n)]
-        indegree = [0 for _ in range(n)]
+        adj = [set() for _ in range(n)]
 
         for start, end in edges:
-            in_adj[end].add(start)
-            indegree[end] += 1
+            adj[start].add(end)
+            adj[end].add(start)
 
-        # for every node that doesn't have incoming edges, 
-        for node, ins in enumerate(indegree):
-            if ins == 0:
-                roots[node] = node
+        visited = set()
+        counter = 0
 
-        # union find
+        def dfs(i: int) -> None:
+            visited.add(i)
+
+            for neighbor in adj[i]:
+                if neighbor not in visited:
+                    dfs(neighbor)
+
         for i in range(n):
-            if roots[i] != N_MAX:
-                continue
-            
-            for adj in in_adj[i]:
-                roots[i] = min(roots[i], roots[adj])
+            if i not in visited:
+                dfs(i)
 
-        return len(list(set(roots)))  # return length of list of all unique values
+                counter += 1
+
+        return counter
