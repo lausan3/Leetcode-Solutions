95f2ed5bc74f70dcf2a48bcaf2a8dc052a7faea1
Anthony <92795688+lausan3@users.noreply.github.com>
2026-01-15 15:38:28 -0500

    ADD All Paths from Source Lead to Destination

--- All Paths from Source Lead to Destination/All Paths from Source Lead to Destination.py
+++ All Paths from Source Lead to Destination/All Paths from Source Lead to Destination.py
@@ -1,27 +1,62 @@
 class Solution:
     def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        """
+        Standard DFS Approach:
+        """
+        def dfs(i):
+            if len(graph[i]) == 0:
+                return i == destination
+            # Seen this node. Is a cycle.
+            elif visited[i] == 1:
+                return False
+            # Processed this node before, which if hasn't returned False, means the path is good.
+            elif visited[i] == 2:
+                return True
+            else:
+                visited[i] = 1
+                for j in graph[i]:
+                    if not dfs(j):
+                        return False
+                visited[i] = 2
+                return True
+        
+        graph = collections.defaultdict(set)
+        for u, v in edges:
+            graph[u].add(v)
+        visited = [0 for _ in range(n)]
+        return dfs(source)
+        """
+        Modified Kahn's Algorithm Approach:
+        Using Kahn's Algorithm from destination rather than source, we can traverse all paths at once.
+        Instead of indegree though, we check outdegree.
+
+        If we have any cycles we break rule 3.
+        
+        Time: O(V+E)
+        Space: O(V+E)
+        """
+        """
         adj = { i : set() for i in range(n) }
+        outdegree = [0] * n
 
         for out, to in edges:
-            adj[out].add(to)
+            if out == destination: return False  # No outgoing edges from destination allowed
 
-        visited = set()
+            adj[to].add(out)
+            outdegree[out] += 1
 
-        def dfs(cur: int):
-            if len(adj[cur]) == 0:
-                return cur == destination
+        q = deque([destination])
 
-            if cur in visited:
-                return False
+        while q:
+            node = q.popleft()
+
+            if node == source:  # found path that led to destination
+                return True
             
-            visited.add(cur)
+            for neighbor in adj[node]:
+                outdegree[neighbor] -= 1
+                if outdegree[neighbor] == 0:
+                    q.append(neighbor)
 
-            for new in adj[cur]:
-                if not dfs(new):
-                    return False
-
-            visited.remove(cur)
-
-            return True
-
-        return dfs(source)
+        return False
+        """
